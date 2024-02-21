aa9070891ae3541f65a946a39f796f2ee82fc672
Kam <58607149+spyderkam@users.noreply.github.com>
2024-02-21 09:55:24 -0600

    Update run_split_by_size.py

--- run_split_by_size.py
+++ run_split_by_size.py
@@ -2,11 +2,10 @@ from string_manipulator import Text     # As of now string_manipulator only has 
 
 
 file = input("Enter the path to the file you are trying to split: ")
-last_dot_index = file.rfind(".")
-
-text = Text(file)
-
-extension = file[last_dot_index+1::]
 size = int(input("What is size of the new files you want (in bytes): "))
+last_dot_index = file.rfind(".")
+text = Text(file)
+extension = file[last_dot_index+1::]
+
 
 text.split_by_size(size, extension)
