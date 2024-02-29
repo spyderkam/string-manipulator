974ba86d89bf594e81269b3b2c96ea0adaa6cfe6
Kam <58607149+spyderkam@users.noreply.github.com>
2024-02-29 14:39:01 -0600

    02/29/2024 Version

--- run_find_string.py
+++ run_find_string.py
@@ -2,30 +2,38 @@ from glob import glob
 from string_manipulator import Text     # As of now string_manipulator only has Text class.
 
 
-word = input("What is the string you want to find? ").lower()
-o_or_m = input(f"Do you want to find the string '{word}' in just one file or multiple files with a specific extension? Enter 'o' for one or 'm' for multiple: ")
+word = input("What is the string you want to find? ")
+o_or_m = input(f"Do you want to find the string '{word}' in just one file or multiple files that are splitted from one file? Enter 'o' for one or 'm' for multiple: ")
 
 
 if o_or_m == 'o':
     file_path = input("Enter the path to the file of interest: ")
     with open(file_path, "r") as f:
-        datum = f.read().lower()
+        datum = f.read()
 
     text = Text(datum)
     finder = text.find_string
     
-    print(f"The string '{word}' appears '{finder(word)[0]}' times in '{file_path}' at positions '{finder(word)[1]}'.")
+    pos_out_yn = input(f"Do you want to output each position of the first charachter of the substring {word} in the string? Enter 'y' for yes or 'n' for no: ").lower()
+    if pos_out_yn == 'y':
+        print(f"The string '{word}' appears '{finder(word)[0]}' times in '{file_path}' at positions '{finder(word)[1]}'.")
+    elif pos_out_yn == 'n':
+        print(f"The string '{word}' appears '{finder(word)[0]}' times in '{file_path}'.")
+    else:
+        raise ValueError("YOU MUST ENTER 'y' FOR YES OR 'n' FOR NO!")
+
 elif o_or_m == 'm':
     ext = input("Enter the type of the files: ").lower()
+    fname = input(f"Enter the name of the files in which you want find the substring `{word}` in WITHOUT entering the underscore, number, or type that follows them: ")
     this_dir_yn = input("Are the files in the current directory? Enter 'y' for yes or 'n' for no: ").lower()
-   
+
     if this_dir_yn == 'n':
-        which_dir = input("Enter the path to the directory which the {ext} files are in: ")
-        files = sorted(glob(f"{which_dir}/*.{ext}"))     # Not sure of it needs to be sorted but it can't hurt...
+        which_dir = input("Enter the path to the directory which the files are in: ")
+        files = sorted(glob(f"{which_dir}/{fname}_*.{ext}"))     # Not sure of it needs to be sorted but it can't hurt...
         if len(files) == 0: raise ValueError(f"Something went wrong, no '{ext}' type files found in '{which_dir}' directory!")
     elif this_dir_yn == 'y':
-        files = sorted(glob(f"*.{ext}"))                 # Not sure of it needs to be sorted but it can't hurt...
-        if len(files) == 0: raise ValueError(f"Something went wrong, no '{ext}' type files found in this directory!")    
+        files = sorted(glob(f"{fname}_*.{ext}"))                 # Not sure of it needs to be sorted but it can't hurt...
+        if len(files) == 0: raise ValueError(f"Something went wrong, no '{ext}' type files found in this directory!")
     else:
         raise ValueError("YOU MUST ENTER 'y' FOR YES OR 'n' FOR NO!")
 
@@ -35,7 +43,7 @@ elif o_or_m == 'm':
         
     for file in files:
         with open(file, "r") as f:
-            datum = f.read().lower()
+            datum = f.read()
         texts.append( Text(datum) )
     
     for text in texts:
