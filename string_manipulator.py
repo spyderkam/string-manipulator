c6b0b7fb99c4d841b9eb8e83008b3b31534a744a
Kam <58607149+spyderkam@users.noreply.github.com>
2024-03-05 15:30:28 -0600

    Update string_manipulator.py

--- string_manipulator.py
+++ string_manipulator.py
@@ -123,11 +123,13 @@ class ExSpread:
             temp_lines = f.readlines()
             for lineNo_m1, line in enumerate(temp_lines):     # lineNo_p1 = 'line number' - 1
                 if line.strip() == s:
-                    rows.extend([temp_lines[lineNo_m1+1].strip().split(),
-                                temp_lines[lineNo_m1+2].strip().split(),
-                                temp_lines[lineNo_m1+3].strip().split()])
+                    rows.extend([
+                                 temp_lines[lineNo_m1+1].strip().split(),
+                                 temp_lines[lineNo_m1+2].strip().split(),
+                                 temp_lines[lineNo_m1+3].strip().split()
+                                ])
 
-        arr = np.zeros((len(rows),len(max(rows,key = lambda x: len(x)))), dtype='<U11')
+        arr = np.zeros((len(rows),len(max(rows, key=lambda x: len(x)))), dtype='<U11')
         arr[:] = " "     # compare with np.nan
 
         for 𝚤,𝚥 in enumerate(rows):
